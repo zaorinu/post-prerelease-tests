36d7c02a430291404980835f45282cbc06a00b61
Luis Antonio <getcyonic+zaorinu@gmail.com>
2025-06-05 23:40:45 +0000

    Removed dark theme and auto-redirect when 404

--- assets/js/theme.js
+++
@@ -1,3 +0,0 @@
-// assets/js/theme.js
-const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-document.documentElement.setAttribute('data-theme', prefersDark ? 'dark' : 'light');
